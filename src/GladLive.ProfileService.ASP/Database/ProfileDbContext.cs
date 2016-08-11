dacb77c20213a6fc8766737e979f86fdb412c297
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-11 13:27:05 -0500

    Add ProfileDbContext

---
+++ src/GladLive.ProfileService.ASP/Database/ProfileDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace GladLive.ProfileService.ASP.Database
+{
+	/// <summary>
+	/// <see cref="DbContext"/> for the <see cref="GladLiveProfileModel"/>.
+	/// </summary>
+	public class ProfileDbContext : DbContext
+	{
+		/// <summary>
+		/// Queryable object that contains the set of profiles.
+		/// </summary>
+		DbSet<GladLiveProfileModel> Profiles { get; set; }
+
+		public ProfileDbContext(DbContextOptions options) 
+			: base(options)
+		{
+
+		}
+	}
+}
