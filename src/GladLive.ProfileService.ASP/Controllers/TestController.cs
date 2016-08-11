67a5a99aabb6bedd6f19a12464fc2c580f3f818e
Andrew Blakely <andrew.blakely@ymail.com>
2016-08-11 02:54:07 -0500

    Setup JWT Authorization

---
+++ src/GladLive.ProfileService.ASP/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+using System.Security.Claims;
+
+namespace GladLive.ProfileService.ASP
+{
+	[Route("api/test")]
+	public class TestController : Controller
+	{
+		private ILogger loggingService { get; }
+
+		public TestController(ILogger<TestController> logger)
+		{
+			loggingService = logger;
+		}
+
+		[Authorize]
+		[HttpGet]
+		public void Get()
+		{
+			foreach(Claim claim in User.Claims)
+				loggingService.LogCritical($"Has Claim: {claim.Type}  {claim.Value}.");
+		}
+	}
+}
---
+++ src/GladLive.ProfileService.ASP/Startup.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.AspNetCore.Identity;
+using OpenIddict;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
+using Microsoft.IdentityModel.Tokens;
+using GladLive.Security.Common;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace GladLive.ProfileService.ASP
+{
+	public class Startup
+	{
+		public IConfigurationRoot Configuration { get; }
+
+		public Startup(IHostingEnvironment env)
+		{
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(env.ContentRootPath)
+				.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+				.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+			builder.AddEnvironmentVariables();
+			Configuration = builder.Build();
+		}
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			
+			services.AddLogging();
+
+			//This adds the MVC core features and GladNet features
+			services.AddMvc();
+		}
+
+		public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+		{
+			//Comment this out for IIS but we shouldn't need it. We'll be running this on
+			//Linux behind AWS Elastic Load Balancer probably
+			//app.UseIISPlatformHandler();
+
+			loggerFactory.AddConsole(LogLevel.Information);
+
+			JwtBearerOptions bearerOptions = new JwtBearerOptions
+			{
+				AutomaticAuthenticate = true,
+				AutomaticChallenge = true,
+				RequireHttpsMetadata = false,
+				TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+				{
+					IssuerSigningKey = new X509SecurityKey(new X509Certificate2(@"Certs/TestCert.pfx")),
+					ValidateIssuerSigningKey = false, //WARNING: This is bad. We should validate the signing key in the future
+					ValidateAudience = false,
+					ValidateIssuer = true,
+					ValidIssuer = "https://localhost:44300/",
+					ValidateLifetime = false //temporary until we come up with a solution
+				},
+			};
+
+			// use jwt bearer authentication
+			app.UseJwtBearerAuthentication(bearerOptions);
+
+			app.UseMvc();
+
+			//We have to register the payload types
+			//We could maybe do some static analysis to find referenced payloads and auto generate this code
+			//or find them at runtime but for now this is ok
+		}
+
+		//This changed in RTM. Fluently build and setup the web hosting
+		public static void Main(string[] args) => new WebHostBuilder()
+			.UseKestrel(options =>
+			{
+				X509Certificate2 cert = new X509Certificate2(@"Certs/certificate.pfx");
+
+				//TODO: Handle cert changes
+				options.UseHttps(new HttpsConnectionFilterOptions() { SslProtocols = System.Security.Authentication.SslProtocols.Tls | System.Security.Authentication.SslProtocols.Tls11 | System.Security.Authentication.SslProtocols.Tls12, ServerCertificate = cert });
+			})
+			.UseUrls(@"https://localhost:44300")
+			.UseContentRoot(Directory.GetCurrentDirectory())
+			.UseStartup<Startup>()
+			.Build()
+			.Run();
+	}
+}
